ed1f6293b5791e7f8e8a874ca53290947f8d0a2a
chanureddy123 <reddychandu1995@gmail.com>
2019-02-28 20:02:47 +0530

    this is fun

---
+++ src/org/foo/object.groovy
@@ -0,0 +1,17 @@
+#!/usr/bin/env groovy
+
+class testing{
+    String test="testing"
+def info(String message){
+    println test
+    test="reddy"
+    println "this is after changing the value "+test
+    String data = message
+    data = "you have entered "+data
+    println data;
+    
+}
+def warning(message) {
+    echo "WARNING: ${message}"
+}
+}
