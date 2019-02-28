8d39c3a204ddf1af182cbf916ac1dd3f407fc3b0
chanureddy123 <reddychandu1995@gmail.com>
2019-02-28 19:56:58 +0530

    test

--- vars/Common.groovy
+++ vars/Common.groovy
@@ -1,8 +1,14 @@
 #!/usr/bin/env groovy
+
+String test="chandu"
 def info(String message){
+    println test
+    test="reddy"
+    println "this is after changing the value "+test
     String data = message
     data = "you have entered "+data
     println data;
+    
 }
 def warning(message) {
     echo "WARNING: ${message}"
