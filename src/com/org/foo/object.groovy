3b0f2a01201032b969f9818e10eb4d91d17e4c1c
chanureddy123 <reddychandu1995@gmail.com>
2019-03-01 11:23:08 +0530

    fasak

--- src/com/org/foo/object.groovy
+++ src/com/org/foo/object.groovy
@@ -3,16 +3,16 @@ package org.foo
 
 class testing{
     String test="testing"
-def info(String message){
-    println test
-    test="reddy"
-    println "this is after changing the value "+test
-    String data = message
-    data = "you have entered "+data
-    println data;
-    
-}
-def warning(message) {
-    echo "WARNING: ${message}"
-}
+    def info(String message){
+        println test
+        test="reddy"
+        println "this is after changing the value "+test
+        String data = message
+        data = "you have entered "+data
+        println data;
+        
+    }
+    def warning(message) {
+        echo "WARNING: ${message}"
+    }
 }
