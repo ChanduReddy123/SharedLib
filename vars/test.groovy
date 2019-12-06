74fa5a89ee606b102c5b3f406ba09242786582d0
Chandu <reddychandu1995@gmail.com>
2019-12-06 21:23:28 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,5 +1,16 @@
 def call(branch){
+  def list= []
   if(branch == 'master'){
-    return 'prod';
+    list[0] = 'prod'
+    list[1] = getIP(branch)
+  }
+}
+
+def getIP(branch){
+  if(branch == 'master'){
+    return '1.2.3.4';
+  }
+  if(branch == 'stg'){
+    return '4.5.6.7';
   }
 }
