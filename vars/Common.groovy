936de3ddf21ed2c89bbd29a4d6f5d6e3ff59bc9d
chandureddy123 <reddychandu1995@gmail.com>
2019-12-06 20:36:52 +0530

    updated the groovy script

--- vars/Common.groovy
+++ vars/Common.groovy
@@ -1,38 +1,33 @@
 #!/usr/bin/env groovy
 
-
-import org.TestingClass
-
-
-
-def info(String call,String params){
-    //println call
-    println "creating the object for testingclass"
-    def testing = new chandu()
-    println testing
-    testing.TestChandu()
-    
-}
 def warning(message) {
     //echo "WARNING: ${message}"
     println "this is in warning method"
     println(message)
 }
 
-def testing(testing)
-{
-    Testingclass Help = new TestingClass()
-    
-    println "this is in testing method "
-    println 'testing'
+
+def getEnvironment(branch){
+    println("given branch is $branch ${branch}");
+    if(branch == 'master'){
+        return 'Prod'
+    }
+    else if(branch == 'Develop'){
+        return 'Dev'
+    }
+    else if(branch == 'Staging'){
+        return 'Stg'
+    }
 }
 
-class chandu{
-    def chandu(){
-
+def getDeploymentIP(branch){
+    if(branch == 'master'){
+        return '1.2.3.4'
     }
-    public TestChandu()
-    {
-        println "this is in chandu class and Test chandu method"
+    else if(branch == 'Develop'){
+        return '5.6.7.8'
+    }
+    else if(branch == 'Staging'){
+        return '3.4.5.6'
     }
 }
