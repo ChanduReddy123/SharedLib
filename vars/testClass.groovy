a89ef1526f39b2527eeb14f1ec0baeb8c741de34
chandureddy123 <reddychandu1995@gmail.com>
2019-12-09 16:36:05 +0530

    updated with the ci skip

--- vars/testClass.groovy
+++
@@ -1,9 +0,0 @@
-public static class testClass{
-   public void Testing(){
-       println("This is for testing ")
-   }
-    public void Testing2(){
-        println "This is for testing 2"
-    }
-
-}
