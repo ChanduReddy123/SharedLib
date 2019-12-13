a3fcaa5f46830f5763743ec5b18739f05f245be0
Chandu <reddychandu1995@gmail.com>
2019-12-13 16:03:20 +0530

    Update GetEnvironment.groovy

--- vars/GetEnvironment.groovy
+++ vars/GetEnvironment.groovy
@@ -1,6 +1,45 @@
-def call(branch){
-  if(branch == 'master'){
-    def list = getIP(branch)
-    return list
-  }
+def call(){
+  return getEnvironment()
 }
+
+def  getEnvironment(){
+  def branch = "${BRANCH_NAME}"
+    def envdetails = [:]
+    if(branch == "master"){
+        envdetails['dbname'] = "masterdb"
+        envdetails['endpoint'] = "masterendpoint"
+        envdetails['password'] = "masterpassword"
+        envdetails['username'] = "masterdbusername"
+        envdetails['port'] = "masterdbport"
+        envdetails['bcpoint'] = "masterbcpoint"
+        return envdetails
+    }
+    else if(branch == "dev"){
+        envdetails['dbname'] = "devdb"
+        envdetails['endpoint'] = "devendpoint"
+        envdetails['password'] = "devpassword"
+        envdetails['username'] = "devdbusername"
+        envdetails['port'] = "devdbport"
+        envdetails['bcpoint'] = "devbcpoint"
+        return envdetails
+    }
+    else if(branch == "stage"){
+        envdetails['dbname'] = "stagedb"
+        envdetails['endpoint'] = "stageendpoint"
+        envdetails['password'] = "stagepassword"
+        envdetails['username'] = "stagedbusername"
+        envdetails['port'] = "stagedbport"
+        envdetails['bcpoint'] = "stagebcpoint"
+        return envdetails
+    }
+  else{
+    return envdetails
+  }
+    
+}
+
+
+///def branch = getBranch()
+//def env = getEnvironment(branch)
+//println(env.endpoint)
+//println(env.dbname)
